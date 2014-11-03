da4deca6675a5b7686f76836dc4259758ea4e5fd
Karthik <karthik@italki.com>
2014-11-04 00:01:19 +0800

    Updated Modern Art UI project

---
+++ gen/com/karthik/rajasethupathy/modernartui/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.karthik.rajasethupathy.modernartui;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
